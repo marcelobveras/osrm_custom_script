06fb5b91415eff7eaaf332d458d664c1f8da6ed7
Marcelo Veras <marcelobveras@gmail.com>
2024-03-22 16:18:36 -0300

    NEW: Create script to run and update OSRM by docker

--- osrm_script.sh
+++ osrm_script.sh
@@ -1,11 +1,30 @@
-#!/bin/bash         
+#!/bin/bash
 	
-map_url=https://download.geofabrik.de/south-america/brazil-latest.osm.pbf
-map_file=brazil-latest.osm.pbf
-git_osrm=https://github.com/Project-OSRM/osrm-backend.git
-osrm_folder=osrm-backend
-# map_file=/home/marcelobveras/Documents/perceptron2.png
-cd /tmp
+map_url="https://download.geofabrik.de/south-america/brazil-latest.osm.pbf"
+map_file="brazil-latest.osm.pbf"
+custom_lua_script="https://raw.githubusercontent.com/marcelobveras/osrm_custom_script/main/custom.lua"
+lua_script_file="custom.lua"
+git_osrm="https://github.com/Project-OSRM/osrm-backend.git"
+osrm_folder="osrm-backend"
+image_name="ghcr.io/project-osrm/osrm-backend"
+update_map=false
+container_prod_name="osrm-backend-prod"
+container_tmp_name="osrm-backend-tmp"
+container_prod_port=5015
+container_tmp_port=5016
+osrm_backend_port=5000
+
+is_updated=false
+
+mkdir ~/osrm-backend 2>null;
+
+cd ~/osrm-backend
+
+if ! [ -f $map_file ]; then
+	wget $map_url
+	touch $map_file
+	update_map=true
+fi
 
 date_file=$(date -r $map_file +%s)
 date_now=$(date +%s)
@@ -15,12 +34,62 @@ date_diff=$((date_diff / 2592000))
 if (( $date_diff > 0 )); then
 	wget $map_url
 	touch $map_file
+	update_map=true
 fi
 
-git clone $git_osrm
+rm $lua_script_file
+wget $custom_lua_script
 
-mv $map_file $osrm_folder
+if [ "$update_map" = true ]; then
+	rm -rf $osrm_folder
+	git clone $git_osrm
+	cp $map_file $osrm_folder
+fi
+
+cp $lua_script_file $osrm_folder
 
 cd $osrm_folder
 
-docker run -t -v "${PWD}:/data" ghcr.io/project-osrm/osrm-backend osrm-extract -p /opt/car.lua /data/$map_file || echo "osrm-extract failed"
+docker container prune -f
+
+if ! docker images "$image_name" | grep -q "$image_name" || [ "$update_map" = true ]; then
+	docker run -t -v "${PWD}:/data/" "$image_name" osrm-extract -p "/data/$lua_script_file" "/data/$map_file" || echo "osrm-extract failed"
+	docker run -t -v "${PWD}:/data/" "$image_name" osrm-partition "/data/brazil-latest.osrm" || echo "osrm-partition failed"
+	docker run -t -v "${PWD}:/data/" "$image_name" osrm-customize "/data/brazil-latest.osrm" || echo "osrm-customize failed"
+else
+	echo "$image_name já é uma imagem e o mapa não foi atualizado"
+fi
+
+if [ "$( docker container inspect -f '{{.State.Status}}' $container_tmp_name 2>/dev/null )" == "running" ] > /dev/null; then
+	docker stop "$container_tmp_name"
+	docker rm "$container_tmp_name"
+fi
+
+if [ "$( docker container inspect -f '{{.State.Status}}' $container_prod_name 2>/dev/null )" == "running" ]; then
+	docker run -t -d -p "$container_tmp_port":"$osrm_backend_port" -v "${PWD}:/data/" --name "$container_tmp_name" "$image_name" osrm-routed --algorithm mld /data/brazil-latest.osrm || echo "osrm-routed failed"
+	url_tmp="http://127.0.0.1:$container_tmp_port/route/v1/driving/13.388860,52.517037;13.385983,52.496891"
+	#echo curl --max-time 5 -s "$url"
+	if wget --timeout=5 -q -O- "$url_tmp" >/dev/null; then
+    		#echo "Connection to $url successful."
+    		docker stop "$container_prod_name"
+    		docker rename "$container_prod_name" old_"$container_prod_name"
+    		docker run -t -d -p "$container_prod_port":"$osrm_backend_port" -v "${PWD}:/data/" --name "$container_prod_name" "$image_name" osrm-routed --algorithm mld /data/brazil-latest.osrm || echo "osrm-routed failed"
+    		docker rm old_"$container_prod_name"
+		docker stop "$container_tmp_name"
+		docker rm "$container_tmp_name"
+	else
+    		echo "Failed setup temporary container."
+	fi
+else
+#	echo "teste"
+	docker run -t -d -p "$container_prod_port":"$osrm_backend_port" -v "${PWD}:/data/" --name "$container_prod_name" "$image_name" osrm-routed --algorithm mld /data/brazil-latest.osrm || echo "osrm-routed failed"
+fi
+
+
+url="http://127.0.0.1:$container_prod_port/route/v1/driving/13.388860,52.517037;13.385983,52.496891"
+
+if wget --timeout=5 -q -O- "$url" >/dev/null; then
+	echo "O servico OSRM está ATIVO e foi ATUALIZADO com sucesso. Pode ser rodado a partir da porta $container_prod_port"
+else
+	echo "SERVICO INATIVO, olhar mensagem de erro"
+fi	
